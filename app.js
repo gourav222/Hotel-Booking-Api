7462b071375b2f0c4d4a3b61de69248223d5cbee
gourav222 <67748004+gourav222@users.noreply.github.com>
2022-06-08 15:55:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const options = {
     },
     servers: [
 {
-        url: "https://hotel-booking-system-api.herokuapp.com/",
+        url: "https://hotel-booking-system-api.herokuapp.com",
       },
     ],
   },
